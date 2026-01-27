2705ce42a7356819d9e9c41514b5b54241a80f6b
DgetYoung <90117432+DgetYoung@users.noreply.github.com>
2026-01-27 11:54:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,9 @@ function addFlowers(){
 function sendTile(t){
   switch (mode){
     case HAND_MODE:
-      closedhand.children[0].src = "graphics/" + ("0" + t).slice(-2) + ".png"
+      closedhand.children[closed.length].src = "graphics/" + ("0" + t).slice(-2) + ".png";
+      console.log("graphics/" + ("0" + t).slice(-2) + ".png");
+      closed.push(t);
       break;
     case CHOW_MODE:
       break;
