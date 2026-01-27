34dac3681fdcd01bbddd671b9e2536259ad397ee
DgetYoung <90117432+DgetYoung@users.noreply.github.com>
2026-01-27 08:25:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-for (let i = 0, i < 14, i++){
-  var tile = document.createElement("img");
-  img.src = "99.png";
-  img.style.height = "26px";
-  img.style.width = "20px";
-  var closedhand = document.getElementByID("closedhand");
-  closedhand.appendChild(img);
+function initialize(){
+  for (let i = 0, i < 14, i++){
+    var tile = document.createElement("img");
+    img.src = "99.png";
+    img.style.height = "26px";
+    img.style.width = "20px";
+    var closedhand = document.getElementByID("closedhand");
+    closedhand.appendChild(img);
+  }
 }
