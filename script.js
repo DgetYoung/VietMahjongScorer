de271d5324391d795cf80246a0a57a33bbe6fcb9
DgetYoung <90117432+DgetYoung@users.noreply.github.com>
2026-01-27 08:20:49 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 0, i < 14, i++){
+  var tile = document.createElement("img");
+  img.src = "99.png";
+  img.style.height = "26px";
+  img.style.width = "20px";
+  var closedhand = document.getElementByID("closedhand");
+  closedhand.appendChild(img);
+}
