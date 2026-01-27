a1b81c4ad41f02b05e730533fb29e41c1ae89830
DgetYoung <90117432+DgetYoung@users.noreply.github.com>
2026-01-27 08:29:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function initialize(){
   for (let i = 0, i < 14, i++){
     var tile = document.createElement("img");
-    img.src = "99.png";
-    img.style.height = "26px";
-    img.style.width = "20px";
+    tile.src = "99.png";
+    tile.style.height = "26px";
+    tile.style.width = "20px";
     var closedhand = document.getElementByID("closedhand");
     closedhand.appendChild(img);
   }
